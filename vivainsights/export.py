e4426a5f8e2999b23884dfef7535e1eb033c956c
Martin Chan <martinchan53@gmail.com>
2023-06-19 19:33:17 +0100

    fix: clean plot saving method

--- vivainsights/export.py
+++ vivainsights/export.py
@@ -3,14 +3,13 @@
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 import pandas as pd
+import matplotlib.pyplot as plt
 from datetime import datetime
 
-def export(data,
+def export(x,
            file_format='clipboard',
            path='insights export',
-           timestamp=True,
-           width=12,
-           height=9):
+           timestamp=True):
            
         """
         Exports the data to the specified file format and saves it to the specified filename.
@@ -18,12 +17,10 @@ def export(data,
         images. By default, `export()` copies a data frame to the clipboard.
         
         Args:
-            data (dataframe): The dataframe to export.
+            x (dataframe or matplotlib figure object): The object to export, which can be a data frame or a matplotlib figure object.
             file_format (csv/png/svg/jpeg/pdf/clipboard): Character string specifying the method of export.
             path (str, optional): If exporting a file, enter the path and the desired file name. Defaults to "insights export".
             timestamp (bool, optional): Logical vector specifying whether to include a timestamp in the file name. Defaults to True.
-            width (int, optional): Width of the plot. Defaults to 12.
-            height (int, optional): Height of the plot. Defaults to 9.
         Return: A different output is returned depending on the value passed to the `file_format`
                 Argument:
                   - `"clipboard"`: no return - data frame is saved to clipboard.
@@ -38,20 +35,24 @@ def export(data,
         if timestamp == True:
             newpath = f"{path} {datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
         else:
-            newpath =  path
+            newpath = path
+            
         # Export option: CSV
-        if  file_format == "csv":
+        #TODO: Check if is data frame
+        if file_format == "csv":
             newpath = f"{newpath}.csv"
-            data.to_csv(newpath, index=False)
+            x.to_csv(newpath, index=False)
 
-        # Export option: png,svg,jpeg,pdf        
-        elif  file_format in ["png", "svg", "jpeg", "pdf"]:
+        # Export option: png,svg,jpeg,pdf
+        #TODO: Check if this works for matplotlib figures
+        elif file_format in ["png", "svg", "jpeg", "pdf"]:
             newpath = f"{newpath}.{ file_format}"
-            data.save(newpath, width= width, height= height)
+            x.savefig(newpath) # matplotlib.savefig
 
         # clipboard export
-        elif  file_format == "clipboard":          
-            data.to_clipboard()                      
+        #TODO: Check if is data frame
+        elif file_format == "clipboard":          
+            x.to_clipboard()                      
             print("Data frame copied to clipboard.\nYou may paste the contents directly to Excel.")
         else:
             raise ValueError("Please check inputs.")        
