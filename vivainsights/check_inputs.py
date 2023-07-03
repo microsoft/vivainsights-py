88d2ad863868576c22ae0eeb5a6247b64b53fade
Martin Chan <martinchan53@gmail.com>
2023-07-03 12:07:58 +0100

    docs: update check_inputs documentation

--- vivainsights/check_inputs.py
+++ vivainsights/check_inputs.py
@@ -2,8 +2,19 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+'''The function `check_inputs` checks if the required variables are present in the given data and
+raises an error if any of them are missing.
+'''
+import pandas as pd
 
-def check_inputs(data, requirements):
+def check_inputs(data: pd.DataFrame, requirements: str):
+  """
+  Args:
+    data (df): The `data` parameter is expected to be a pandas DataFrame object that contains the data to be checked.
+    requirements (str): The `requirements` parameter is a list of variables that are required to be present in the `data` object. 
+    
+  The function `check_inputs` checks if each variable in the `requirements` list is present as a column in the `data` object. If any variable is missing, it raises an error.
+  """
   # Check if the required variables are in the data
   # Raise an error if not
   for var in requirements:
