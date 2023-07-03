83c4d1397d3f9703728b63fb18a1e785bc7472c4
Martin Chan <martinchan53@gmail.com>
2023-07-03 18:56:05 +0100

    fix: github actions

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # The setup.py file will contain information about the package
 # specifically the name of the package, its version, platform-dependencies, etc.
 from setuptools import setup, find_packages
+
+with open('requirements.txt', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
 setup(name='vivainsights',
       version='0.1.0',
       url='https://github.com/microsoft/vivainsights',
-      license='MIT',
+      license= 'MIT',
       author='Martin Chan',
       author_email='martin.chan@microsoft.com',
       description='Analyze and Visualize data from Microsoft Viva Insights',
@@ -12,4 +16,7 @@ setup(name='vivainsights',
       long_description=open('README.md').read(),
       zip_safe=False,
       include_package_data=True,
-      package_data={'vivainsights': ['data/*.csv']},)
+      package_data={'vivainsights': ['data/*.csv']},
+      install_requires=requirements,
+      dependencies='dynamic'      
+      )
