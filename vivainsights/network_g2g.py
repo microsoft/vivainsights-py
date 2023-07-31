68634ff191beb141fed938923124fdee11fc4b52
Imran Ahmadi <t-iahmadi@microsoft.com>
2023-07-31 16:06:25 +0100

    feat: add data manipulation to plot_data.

---
+++ vivainsights/network_g2g.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+"""
+This module returns a network plot given a data frame containing a group-to-group query.
+"""
