740a396e092af6ab7dfd8400e14524ac58d839a9
Imran Ahmadi <t-iahmadi@microsoft.com>
2023-08-01 10:31:13 +0100

    feat: return tidy matrix and long table

--- vivainsights/network_g2g.py
+++ vivainsights/network_g2g.py
@@ -7,8 +7,9 @@ This module returns a network plot given a data frame containing a group-to-grou
 """
 import pandas as pd
 import igraph as ig
+import matplotlib.pyplot as plt 
+import seaborn as sns
 import re
-
 
 def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algorithm="fr", node_colour="lightblue", exc_threshold=0.1, org_count=None, subtitle="Collaboration Across Organizations", return_type="plot"):
     if primary is None:
@@ -27,7 +28,6 @@ def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algo
     #Warn if 'Within Group' is not in the data
     if "WithinGroup" not in data[secondary].unique():
         print("Warning: WithinGroup variable is not found in the " + secondary + " variable. The analysis may be excluding in-group collaboration.")
-    
 
     #Run plot_data
     plot_data = data.rename(columns = {primary: "PrimaryOrg", secondary: "SecondaryOrg", metric: "Metric"})
@@ -37,4 +37,13 @@ def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algo
     plot_data = plot_data.apply(lambda func: func.assign(metric_prop = func.Metric / func.Metric.sum())).reset_index(drop = True)
     plot_data = plot_data.loc[:, ["PrimaryOrg", "SecondaryOrg", "metric_prop"]]
 
+    if return_type == "table":
+
+        #return a 'tidy' matrix
+        return plot_data.pivot(index = "PrimaryOrg", columns = "SecondaryOrg", values = "metric_prop")
+    
+    elif return_type == "data":
+
+        #return long table
+        return plot_data
     
