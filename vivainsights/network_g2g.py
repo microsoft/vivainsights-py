28958b9976bc54cd0666672294ed883ce9a9ddad
ia404 <imran.a10@outlook.com>
2023-08-02 14:39:32 +0100

    feat: node_colour value should reflect on the cairoplot object.

--- vivainsights/network_g2g.py
+++ vivainsights/network_g2g.py
@@ -9,6 +9,7 @@ import pandas as pd
 import igraph as ig
 import numpy as np
 import re
+import random
 
 def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algorithm="fr", node_colour="lightblue", exc_threshold=0.1, org_count=None, subtitle="Collaboration Across Organizations", return_type="plot"):
     if primary is None:
@@ -66,7 +67,6 @@ def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algo
                 .loc[:, "n"]
                 .tolist()
             )
-
         else:
             #imputed size if not specified
              g.vs['org_size'] = (
@@ -76,7 +76,7 @@ def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algo
                 .loc[:, 'n']
                 .tolist()
             )
-             
+                     
         #plot object
         g = g.simplify() 
         plot_obj = ig.plot(
@@ -88,7 +88,7 @@ def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algo
             vertex_label=g.vs["name"],
             vertex_size=g.vs["org_size"],
             vertex_frame_width=0,          
-            vertex_color=node_colour,  
+            vertex_color=setColor(node_colour, g.vs["name"]),  
             edge_width=mynet_em["metric_prop"] * 1,
             edge_color="grey",
             edge_alpha=0.5,
@@ -99,10 +99,34 @@ def network_g2g(data, primary=None, secondary=None, metric="Meeting_Count", algo
 
         if return_type == "network":
             return g #return 'igraph' object
-        
-        #TODO: Custom Node Colours 
-        #TODO: Auto assign colours
-        #TODO: Return 'plot' object
-
+        else:
+            return plot_obj.save('plot.png') #return 'cairoplot' object
     else:
         raise ValueError("Please enter a valid input for 'return'.")
+    
+def setColor(node_colour, org):
+    if isinstance(node_colour, str) and len(node_colour) > 1:
+        #check if node_colour is set to default
+        if node_colour == "lightblue":
+            node_colour = "lightblue"
+        elif node_colour == "vary": #generate a random colour for each node in the network
+            node_colour = [f"#{random.randint(0, 0xFFFFFF):06x}" for _ in range(len(org))]
+        else:
+            node_colour = node_colour #use the colour provided
+    elif isinstance(node_colour, dict): #use dictionary to map each node to a colour
+        node_colour = {node: colour for node, colour in node_colour.items()} #key is node, value is colour
+        for node, colour in node_colour.items():
+            if colour == "#000000":
+                node_colour[node] = "#000000"
+            elif colour == "#808080":
+                node_colour[node] = "#808080"
+            elif colour == "lightblue":
+                node_colour[node] = "lightblue"
+            else:
+                node_colour[node] = colour
+        node_colour = [node_colour.get(o, "lightblue") for o in org]
+    else: #default colour
+        node_colour = "lightblue"
+
+    return node_colour
+    
