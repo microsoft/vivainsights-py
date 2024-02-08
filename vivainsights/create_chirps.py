fd20388dc0a7eb68a8096edc01b6cdc971cfe243
Martin Chan <martinchan53@gmail.com>
2024-02-08 15:43:14 +0000

    feat: remove unused code and add Cohen's d

--- vivainsights/create_chirps.py
+++ vivainsights/create_chirps.py
@@ -108,114 +108,15 @@ def test_ts(data: pd.DataFrame,
             
     return grouped_data_list
 
-    # For each HR attribute and metric combination, for each group within HR attribute, calculate:
-    #  - group mean based on the latest date
-    #  - group 4MA
-    #  - group 12MA
-    #  - all the time group average
-    #  - group standard deviation
-    #  - group rank
-    #  - group count - total number of groups in HR attribute
-    #  - average 4 week rank
-    #  - average 12 week rank
-    #  - group current percentile
-    #  - group percentile change over 12 weeks
-    #  - test upper bollinger
-    #  - stdev above all time
-    #  - flip 4MA 12MA
-    #  - group 4MA exceeding benchmark 
-
-
-def test_ts_person(
-    data: pd.DataFrame,
-    metrics: list,
-    min_group: int = 5
-    ):
-    """
-    This function takes in a DataFrame representing a Viva Insights person query and returns a list of DataFrames containing the results of the trend test.
-    
-    The function identifies the latest date in the data, and defines two periods: the last four weeks and the last twelve weeks. 
-    
-    It then initializes a list of exception metrics where lower values and downward trends are notable. 
-    
-    This version does not group by HR variable groups, and returns data frames at a person-week level.   
-    """    
-    
-    # Define date windows
-    latest_date = data['MetricDate'].max()
-    four_weeks_ago = latest_date - timedelta(weeks=4)
-    twelve_weeks_ago = latest_date - timedelta(weeks=12)
-    
-    short_period = 4
-    long_period = 12
-    
-    # Identify list of exception metrics where lower values and downward trends are notable
-    exception_metrics = [
-        'Meeting_hours_with_manager_1_on_1', 
-        'Meetings_with_manager_1_on_1', 
-        'Total_focus_hours'
-    ]
-    
-    metric_columns = metrics
-    ranking_order = {metric: 'low' if metric in exception_metrics else 'high' for metric in metric_columns}
-
-    # Initialize an empty list for storing DataFrames
-    results_data_list = []
-
-    for each_metric in metrics:
-        
-        # Initialize an empty DataFrame for group metrics
-        person_metrics = data[['PersonId', 'MetricDate', each_metric]].copy()
-        
-        # Filter data for the specific time periods
-        person_metrics = person_metrics[(person_metrics['MetricDate'] >= twelve_weeks_ago) & (person_metrics['MetricDate'] <= latest_date)]      
-
-        # Calculate moving averages and standard deviation at a person-level, for each metric  
-        person_metrics[each_metric + '_mean'] = person_metrics.groupby('PersonId')[each_metric].apply(lambda x: x.mean()).reset_index(level=0, drop=True)
-        person_metrics[each_metric + '_4MA'] = person_metrics.groupby('PersonId')[each_metric].apply(lambda x: x.rolling(window=4, min_periods=1).mean()).reset_index(level=0, drop=True)
-        person_metrics[each_metric + '_12MA'] = person_metrics.groupby('PersonId')[each_metric].apply(lambda x: x.rolling(window=12, min_periods=1).mean()).reset_index(level=0, drop=True)
-        person_metrics[each_metric + '_Stdev'] = person_metrics.groupby('PersonId')[each_metric].apply(lambda x: x.rolling(window=long_period, min_periods=1).std()).reset_index(level=0, drop=True)
-        
-        # Calculate the 4MA and 12MA for the previous week
-        person_metrics['Last_Week_4MA_' + each_metric] = person_metrics.groupby('PersonId')[each_metric + '_4MA'].shift(1).reset_index(level=0, drop=True)
-        person_metrics['Last_Week_12MA_' + each_metric] = person_metrics.groupby('PersonId')[each_metric + '_12MA'].shift(1).reset_index(level=0, drop=True)
-        
-        # Determine flip condition
-        # For exceptions: if 4MA is greater than 12MA but not the case last week, True
-        # Otherwise, if 4MA is less than 12MA but not the case last week, True        
-        person_metrics['4MA_Flipped_12MA_' + each_metric] = person_metrics.apply(
-            lambda row: (row[each_metric + '_4MA'] > row[each_metric + '_12MA'] and 
-                         row['Last_Week_4MA_' + each_metric] <= row['Last_Week_12MA_' + each_metric])
-            if each_metric not in exception_metrics
-            else (row[each_metric + '_4MA'] < row[each_metric + '_12MA'] and
-                  row['Last_Week_4MA_' + each_metric] >= row['Last_Week_12MA_' + each_metric]), axis=1
-        ).reset_index(level=0, drop=True)
-       
-        # Calculate the weekly average for the metric
-        weekly_avg = person_metrics.groupby('MetricDate')[each_metric].transform('mean')
-        person_metrics['WeeklyAvg_' + each_metric] = weekly_avg
-
-        # Determine if the person's metric exceeded the weekly average, reverse logic for exception metrics
-        person_metrics[each_metric + '_Exceeds_Avg'] = (
-            person_metrics[each_metric] > weekly_avg) if each_metric not in exception_metrics else (person_metrics[each_metric] < weekly_avg)
-       
-        # `Exceeds_Threshold` omitted as this is captured in benchmark test        
-        results_data_list.append(person_metrics)
-    
-    return results_data_list
-    print('Trend test complete')
-
-
 def test_int_bm(data: pd.DataFrame,
                   metrics: list,
                   hrvar: list = ["Organization", "SupervisorIndicator"],
                   min_group: int = 5):
     """
-        
     Performs an internal benchmark test on each metric and HR variable combination in the provided DataFrame.
 
-    The function calculates the mean, standard deviation, and number of unique employees for each group and for the entire population. It then performs a two-sample t-test to compare the group mean to the population mean for each metric.
-    
+    The function calculates the mean, standard deviation, and number of unique employees for each group and for the entire population. It also calculates Cohen's d, a measure of effect size.
+
     A list of data frames is returned.
 
     Parameters
@@ -235,16 +136,14 @@ def test_int_bm(data: pd.DataFrame,
     Returns
     -------
     list
-        A list of DataFrames. Each DataFrame contains the results of the internal benchmark test for one metric. The DataFrame includes the original data, the group and population statistics, the p-value of the t-test, and a boolean column indicating whether the result is statistically significant at the 0.05 level.
+        A list of DataFrames. Each DataFrame contains the results of the internal benchmark test for one metric. The DataFrame includes the original data, the group and population statistics, and Cohen's d:
         - the mean, standard deviation, and number of employees for each group
         - the population mean and standard deviation for each metric
-        - the p-value, indicating whether the difference between the group mean and the population mean is statistically significant
-        - a column indicating whether the result is statistically significant
-        - a column indicating the type of the test
+        - Cohen's d, a measure of effect size
 
     Notes
     -----
-    The function uses the `vi.create_rank_calc` function to calculate the rank for each metric. It also appends the full population mean, standard deviation, and number of unique employees to the DataFrame. The two-sample t-test is performed using the `stats.ttest_ind_from_stats` function from the SciPy library.
+    The function uses the `vi.create_rank_calc` function to calculate the rank for each metric. It also appends the full population mean, standard deviation, and number of unique employees to the DataFrame. Cohen's d is calculated as the difference between the group mean and the population mean, divided by the pooled standard deviation.
     """
     
     grouped_data_benchmark_list = []
@@ -268,18 +167,10 @@ def test_int_bm(data: pd.DataFrame,
         bm_data['pop_std_' + each_metric] = data[each_metric].std()
         bm_data['pop_n'] = data['PersonId'].nunique()
 
-        # Perform the t-test and add the p-values to the DataFrame
-        bm_data['p_value'] = bm_data.apply(lambda row: stats.ttest_ind_from_stats(
-            mean1=row['metric'], std1=row['sd'], nobs1=row['n'],
-            mean2=row['pop_mean_' + each_metric], std2=row['pop_std_' + each_metric], nobs2=row['pop_n']
-        )[1], axis=1)
-
-        # Add a column indicating whether the result is statistically significant
-        alpha = 0.05  # Set your significance level
-        bm_data['is_significant'] = bm_data['p_value'] < alpha
-
-        # Add a column indicating the type of the test
-        bm_data['test_type'] = 'Two-sample t-test'
+        # Calculate Cohen's d between means of group and benchmark population
+        # the magnitude of the difference between two means in terms of standard deviation units
+        # d = (M1 - M2) / sqrt((s1^2 + s2^2) / 2)
+        bm_data['cohen_d'] = (bm_data['metric'] - bm_data['pop_mean_' + each_metric]) / np.sqrt((bm_data['sd'] ** 2 + bm_data['pop_std_' + each_metric] ** 2) / 2)
         
         grouped_data_benchmark_list.append(bm_data)
     
