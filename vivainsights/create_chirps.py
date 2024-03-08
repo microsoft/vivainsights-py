622468af032d79000b2bc78608135d9bedcfaa43
Martin Chan <martinchan53@gmail.com>
2024-03-08 00:00:11 +0000

    feat: briefs and docstrings

--- vivainsights/create_chirps.py
+++ vivainsights/create_chirps.py
@@ -8,15 +8,109 @@ import vivainsights as vi
 import pkg_resources
 
 def create_chirps(data: pd.DataFrame,
-                  metrics: list,
+                  brief: str = None,
+                  metrics: list = None,
                   hrvar: list = ["Organization", "SupervisorIndicator"],
+                  bm_hrvar: list = ["FunctionType", "SupervisorIndicator"],
                   min_group: int = 5,
                   bp = {},
                   return_type: str = 'table'):
     
+    """
+    Name
+    ----
+    create_chirps
+    
+    Description
+    ------------    
+    This function creates headlines based on the provided data, brief, metrics, and other parameters.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The DataFrame containing the Person Query data to run the data on. 
+    
+    brief : str, optional
+        The brief for which the metrics are to be returned. Defaults to None.
+        Valid briefs are:
+        - Collaboration patterns
+        - After-hours work
+        - Working on weekends
+        - External connection
+        - Inefficient meetings
+        - Meeting scheduling
+        - Time in meetings
+        - Punctuality
+        - Advanced notice
+        - Meeting size and duration
+        - Network sizes
+        - Time to focus
+        - Focus time adoption
+        - Hybrid work
+        - Manager coaching time
+        - Healthy rhythms
+        - Shifting directions
+        - Confidence in leadership
+        - Alignment with career goals
+        - Urgency
+        - Visibility
+    
+    metrics : list, optional
+        A list of metrics to be used. Defaults to None.
+
+    hrvar : list
+        The HR variables to group by. Defaults to ['Organization', 'SupervisorIndicator'].
+    
+    bm_hrvar : list, optional
+        A list of benchmark human resource variables. Defaults to ["FunctionType", "SupervisorIndicator"].
+    
+    min_group : int, optional
+        The minimum group size. Defaults to 5.
+    
+    bp : dict, optional
+        Best practice parameters. Defaults to {}.
+    
+    return_type : str, optional
+        The type of return value. Defaults to 'table'.
+
+    Returns
+    -------
+    pd.DataFrame: A data frame containing the chirps.
+
+    Raises
+    ------
+    ValueError: If neither `brief` nor `metrics` are provided.
+    """
+    
+    # Either `brief` or `metrics` must be provided
+    if not brief and not metrics:
+        raise ValueError('Either `brief` or `metrics` must be provided.')
+    
+    # If `brief` is not None, run `brief_to_metrics`, which returns a list of metrics
+    # Otherwise, set up an empty list
+    if brief:
+        all_metrics = brief_to_metrics(brief) 
+    elif not brief:
+        all_metrics = []
+     
+    # Set up empty list for merging
+    if not metrics:
+        metrics = []    
+        
+    # If values are provided to both `brief` and 'metrics', then the final metrics will be combined. 
+    all_metrics = list(set(all_metrics + metrics))
+    
     # 1. Trend test - 4 weeks vs 12 weeks -------------------------------------
     
-    list_ts = vi.test_ts(data = data, metrics = metrics, hrvar = hrvar, min_group = min_group, bp = bp, return_type = 'headlines')
+    list_ts = vi.test_ts(
+        data = data,
+        metrics = metrics,
+        hrvar = hrvar,
+        min_group = min_group,
+        bp = bp,
+        bm_hrvar = bm_hrvar,
+        return_type = 'headlines'
+        )
     
     # 2. Internal benchmark test ----------------------------------------------
     
@@ -24,14 +118,20 @@ def create_chirps(data: pd.DataFrame,
         data = data,
         metrics = metrics,
         hrvar = hrvar,
-        bm_hrvar = ["FunctionType", "SupervisorIndicator"], 
+        bm_hrvar = bm_hrvar, 
         min_group = min_group,
         return_type = 'headlines'
         )
     
     # 3. Best practice test ---------------------------------------------------
     
-    list_bp = vi.test_best_practice(data = data, metrics = metrics, hrvar = hrvar, bp = bp, return_type = 'headlines')
+    list_bp = vi.test_best_practice(
+        data = data,
+        metrics = metrics,
+        hrvar = hrvar,
+        bp = bp,
+        return_type = 'headlines'
+        )
     
     # All the headlines -------------------------------------------------------
     
@@ -46,15 +146,26 @@ def create_chirps(data: pd.DataFrame,
     # Output - return all the headlines - append the interesting score
     
     # If `TestType == 'Internal Benchmark'`, then multiply `Interest_Score` by 0.9
-    all_headlines['Interest_Score'] = np.where(all_headlines['TestType'] == 'Internal Benchmark', all_headlines['Interest_Score'] * 0.9, all_headlines['Interest_Score'])
+    all_headlines['Interest_Score'] = np.where(
+        all_headlines['TestType'] == 'Internal Benchmark',
+        all_headlines['Interest_Score'] * 0.9,
+        all_headlines['Interest_Score']
+        )    
     
     # If `TestType == 'Best Practice'`, then multiply `Interest_Score` by 0.8
-    all_headlines['Interest_Score'] = np.where(all_headlines['TestType'] == 'Best Practice', all_headlines['Interest_Score'] * 0.8, all_headlines['Interest_Score'])
+    all_headlines['Interest_Score'] = np.where(
+        all_headlines['TestType'] == 'Best Practice',
+        all_headlines['Interest_Score'] * 0.8,
+        all_headlines['Interest_Score']
+        )
     
     all_headlines['Interest_Score2'] = all_headlines['Interest_Score'] * all_headlines['prop_n']
     
     # `Interest_Score2_minmax` - min-max scaled score
-    all_headlines['Interest_Score2_minmax'] = (all_headlines['Interest_Score2'] - all_headlines['Interest_Score2'].min()) / (all_headlines['Interest_Score2'].max() - all_headlines['Interest_Score2'].min())
+    all_headlines['Interest_Score2_minmax'] = (
+        all_headlines['Interest_Score2'] - all_headlines['Interest_Score2'].min()) / (
+            all_headlines['Interest_Score2'].max() - all_headlines['Interest_Score2'].min()
+            )
     
     # Drop and rename columns
     all_headlines = all_headlines.drop(columns=['prop_n', 'Interest_Score', 'Interest_Score2'])
@@ -63,26 +174,25 @@ def create_chirps(data: pd.DataFrame,
     # Sort 'Interest_Score' in descending order
     all_headlines = all_headlines.sort_values(by='Interest_Score', ascending=False) 
     
-    #TODO: headline selection in order to build a story
-    # Interestingness: time trend > internal benchmark > best practice
-    # Then build in rules for when best practice etc. may trump time trends (e.g. What makes a strong/weak trend?)    
-    
-    
-    
-    
-    # Output - just the interesting headlines
-    # Output - individual tables for each test 
-    # Plot for each interesting headline
-    
-    # print(str(len(list_ts)) + ' number of data frame outputs from trend test')
-    # print(str(len(list_int_bm)) + ' number of data frame outputs from internal benchmark test')
-    # 
-    # combined_list = list_ts + list_int_bm
-    # return combined_list
+    # Return output
     return all_headlines
 
 
-def brief_to_metrics(brief):
+def brief_to_metrics(brief: str):
+    """
+    Name
+    ----
+    brief_to_metrics
+    
+    Description
+    ------------    
+    Accepts a brief and returns a list of metrics associated with the brief.
+    
+    Parameters
+    ----------
+    brief : str
+        The brief for which the metrics are to be returned.
+    """
     
     # Read in the briefs and metrics file
     if(pkg_resources.resource_exists(__name__, 'data/briefs_and_metrics.csv')):
@@ -90,9 +200,16 @@ def brief_to_metrics(brief):
     elif(pkg_resources.resource_exists(__name__, '../data/briefs_and_metrics.csv')):
         stream = pkg_resources.resource_stream(__name__, '../data/briefs_and_metrics.csv')
     else:
-        print('Error: please report issue to repo maintainer')    
+        print('Error: please report issue to repo maintainer')
         
-    brief_metric_dict = stream.groupby('Brief')['Metric'].apply(list).to_dict()
+    briefs_and_metrics = pd.read_csv(stream, encoding='utf-8')
+    
+    # Standardise case for easier matching
+    briefs_and_metrics['Brief'] = briefs_and_metrics['Brief'].str.lower()
+    brief = brief.lower()
+        
+    # Convert to dictionary
+    brief_metric_dict = briefs_and_metrics.groupby('Brief')['Metric'].apply(list).to_dict()
     
     # If 'brief' value is not in the dictionary, it returns an empty list.    
     return brief_metric_dict.get(brief, [])
