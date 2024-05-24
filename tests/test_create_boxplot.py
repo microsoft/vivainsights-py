10705496c153eeb0093c8a15001a4cec9dabe23c
Martin Chan <martinchan53@gmail.com>
2024-05-24 11:57:36 +0100

    chore: add tests

--- tests/test_create_boxplot.py
+++ tests/test_create_boxplot.py
@@ -5,6 +5,8 @@ from vivainsights.create_boxplot import create_boxplot_calc
 from vivainsights.create_boxplot import create_boxplot
 from vivainsights.pq_data import load_pq_data
 import matplotlib.pyplot as plt
+import tracemalloc
+import gc
 
 
 class TestCreateBoxplotViz(unittest.TestCase):
@@ -73,6 +75,24 @@ class TestCreateBoxplot(unittest.TestCase):
         # Check that the DataFrame has the correct columns
         self.assertListEqual(output.columns.tolist(), ['index','group', 'mean', 'median', 'sd', 'min', 'max', 'n'])
 
+    def test_create_boxplot_no_hrvar_returns_plot(self):
+        # Test that a plot is returned when hrvar is None        
+        tracemalloc.start()
+        pq_data = load_pq_data()
+        # Create a box plot of the emails sent with no HR variable
+        output = create_boxplot(
+            data = pq_data,
+            metric = 'Emails_sent',
+            hrvar=None, 
+            return_type='plot'
+            )
+        
+        # garbage collection
+        del pq_data
+        gc.collect()
+        
+        # Check that the output is a matplotlib.figure.Figure object
+        self.assertIsInstance(output, plt.Figure)
         
 if __name__ == '__main__':
     unittest.main()
